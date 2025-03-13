201fa66956f2e522d83a6b4b5f3d519b416bcb6e
elvinkyungu <elvinkyungu.75@gmail.com>
2025-03-13 16:17:23 +0200

    added tailwindcss and gsap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite"
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-
+  css: ['~/assets/css/main.css'],
   modules: [
     '@nuxt/content',
     '@nuxt/eslint',
@@ -11,5 +13,10 @@ export default defineNuxtConfig({
     '@nuxt/image',
     '@nuxt/ui',
     '@nuxt/test-utils'
-  ]
+  ],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 })
