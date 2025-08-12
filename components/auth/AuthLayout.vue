b09af956881bd3c5308135c84e5663fdba0c4328
Elvinkyungu <elvinkyungu.75@gmail.com>
2025-08-12 15:33:30 +0100

    set env variables names

--- components/auth/AuthLayout.vue
+++ components/auth/AuthLayout.vue
@@ -1,3 +1,5 @@
+<script setup lang="ts"></script>
+
 <template>
   <div class="min-h-screen flex">
     <div class="w-full md:w-1/2 flex items-center justify-center p-8 bg-white">
@@ -7,4 +9,3 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
